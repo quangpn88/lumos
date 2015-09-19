83bc9c7127c04f35bfa6bb1af2124ff03f5bfa20
Phan Nhat Quang <quangpn88@gmail.com>
2015-09-19 21:06:25 +0700

    add checking lunch menu

--- lumos.go
+++ lumos.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/tealeg/xlsx"
+	"io/ioutil"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -12,6 +15,10 @@ var (
 )
 
 func main() {
+	ReadMenuFile(FindLunchmenuFile())
+}
+
+func Checklock() {
 	duration := time.Since(dateStart)
 	days := int(duration.Hours() / 24)
 	weekth := days / 7
@@ -21,4 +28,54 @@ func main() {
 	position := weekth % len(systems)
 	fmt.Println("days:" + strconv.Itoa(days) + "  weekth:" + strconv.Itoa(weekth))
 	fmt.Println(systems[position-1])
+}
+
+func FindLunchmenuFile() string {
+	basePath := "./"
+	today := time.Now()
+	basePath += strconv.Itoa(today.Year())
+	monthDirs, err := ioutil.ReadDir(basePath)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	var monthDirName string
+	for _, monthDir := range monthDirs {
+		if strings.Contains(today.Month().String(), monthDir.Name()) {
+			monthDirName = monthDir.Name()
+			basePath += "/" + monthDirName
+			break
+		}
+	}
+	weekFiles, err := ioutil.ReadDir(basePath)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	for _, weekFile := range weekFiles {
+		weekFileName := weekFile.Name()
+		if !strings.Contains(weekFileName, "~$") {
+			indexOfMoth := strings.LastIndex(weekFileName, monthDirName)
+			firstDay := weekFileName[indexOfMoth+len(monthDirName)+1 : indexOfMoth+len(monthDirName)+1+2]
+			firstDayInt, _ := strconv.Atoi(firstDay)
+			if firstDayInt <= today.Day() && today.Day() <= firstDayInt+7 {
+				return basePath + "/" + weekFileName
+			}
+		}
+	}
+	return ""
+}
+
+func ReadMenuFile(filePath string) {
+	xlFile, _ := xlsx.OpenFile(filePath)
+	for _, sheet := range xlFile.Sheets {
+		for rowIndex, row := range sheet.Rows {
+			for cellIndex, cell := range row.Cells {
+				if strings.Contains(cell.String(), "Wednesday") {
+					fmt.Printf("%s\n", cell.String())
+					fmt.Println(sheet.Rows[rowIndex+1].Cells[cellIndex].String())
+					return
+				}
+
+			}
+		}
+	}
 }
