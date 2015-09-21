01c3b217bc7c738d785541648306d29e0b530212
pnquang <quangpn88@gmail.com>
2015-09-21 08:47:25 +0700

    add code

--- lumos.go
+++ lumos.go
@@ -10,28 +10,26 @@ import (
 )
 
 var (
-	systems   = []string{"BID", "BIB", "VISAP", "CASAP", "CONSOLIDATOR"}
+	systems   = []string{"VISAP", "CASAP", "SCARD", "BID", "BIB", "CONSOL"}
 	dateStart = time.Date(2015, time.September, 14, 0, 0, 0, 0, time.UTC)
 )
 
 func main() {
+	Checklog()
 	ReadMenuFile(FindLunchmenuFile())
 }
 
-func Checklock() {
+func Checklog() {
 	duration := time.Since(dateStart)
 	days := int(duration.Hours() / 24)
-	weekth := days / 7
-	if days%7 != 0 {
-		weekth = days/7 + 1
-	}
+	weekth := days/7 + 1
 	position := weekth % len(systems)
 	fmt.Println("days:" + strconv.Itoa(days) + "  weekth:" + strconv.Itoa(weekth))
 	fmt.Println(systems[position-1])
 }
 
 func FindLunchmenuFile() string {
-	basePath := "./"
+	basePath := "X:/"
 	today := time.Now()
 	basePath += strconv.Itoa(today.Year())
 	monthDirs, err := ioutil.ReadDir(basePath)
@@ -56,7 +54,7 @@ func FindLunchmenuFile() string {
 			indexOfMoth := strings.LastIndex(weekFileName, monthDirName)
 			firstDay := weekFileName[indexOfMoth+len(monthDirName)+1 : indexOfMoth+len(monthDirName)+1+2]
 			firstDayInt, _ := strconv.Atoi(firstDay)
-			if firstDayInt <= today.Day() && today.Day() <= firstDayInt+7 {
+			if firstDayInt <= today.Day() && today.Day() < firstDayInt+7 {
 				return basePath + "/" + weekFileName
 			}
 		}
@@ -65,11 +63,12 @@ func FindLunchmenuFile() string {
 }
 
 func ReadMenuFile(filePath string) {
+	today := time.Now()
 	xlFile, _ := xlsx.OpenFile(filePath)
 	for _, sheet := range xlFile.Sheets {
 		for rowIndex, row := range sheet.Rows {
 			for cellIndex, cell := range row.Cells {
-				if strings.Contains(cell.String(), "Wednesday") {
+				if strings.Contains(cell.String(), today.Weekday().String()) {
 					fmt.Printf("%s\n", cell.String())
 					fmt.Println(sheet.Rows[rowIndex+1].Cells[cellIndex].String())
 					return
